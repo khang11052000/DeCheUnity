15dc8ceaf87267a08be2fc4295f129a83856aff8
Khang <nguyenhoangduykhang11052000@gmail.com>
2022-05-23 16:10:08 +0700

    Enemy Rotation

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -12,21 +12,29 @@ public class Enemy : MonoBehaviour
     public float MoveSpeed { get; set; }
     public WayPoint Waypoint { get; set; }
     
+    // Return the current Point Position where this enemy needs to go
     public Vector3 CurrentPointPosition => Waypoint.GetWaypointPosition(_currenWaypointIndex);
     
     private int _currenWaypointIndex;
+    private Vector3 _lastPointPosition;
+
     private EnemyHealth _enemyHealth;
+    private SpriteRenderer _spriteRenderer;
 
     private void Start()
     {
+        _enemyHealth = GetComponent<EnemyHealth>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        
         _currenWaypointIndex = 0;
         MoveSpeed = moveSpeed;
-        _enemyHealth = GetComponent<EnemyHealth>();
+        _lastPointPosition = transform.position;
     }
 
     private void Update()
     {
         Move();
+        Rotate();
         if (CurrentPointPositionReached())
         {
             UpdateCurrentPointIndex();
@@ -49,11 +57,24 @@ public class Enemy : MonoBehaviour
         MoveSpeed = moveSpeed;
     }
 
+    private void Rotate()
+    {
+        if (CurrentPointPosition.x > _lastPointPosition.x)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else
+        {
+            _spriteRenderer.flipX = true;
+        }
+    }
+    
     private bool CurrentPointPositionReached()
     {
         float distanceToNextPointPosition = (transform.position - CurrentPointPosition).magnitude;
         if (distanceToNextPointPosition < 0.1f)
         {
+            _lastPointPosition = transform.position;
             return true;
         }
 
