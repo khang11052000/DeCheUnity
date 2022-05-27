f86a0106ecb2c4165c326da395b2f9c1212640c4
Khang <nguyenhoangduykhang11052000@gmail.com>
2022-05-27 17:56:52 +0700

    Flip tower follow enemy

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -7,11 +7,62 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] private float attackRange = 3f;
 
+    public Enemy CurrentEnemyTarget { get; set; }
+
+    public Vector3 CurrentPos;
+    
     private bool _gameStarted;
+    private List<Enemy> _enemies;
+    private SpriteRenderer[] _spriteRenderer;
 
     private void Start()
     {
         _gameStarted = true;
+        _enemies = new List<Enemy>();
+        _spriteRenderer = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        GetCurrentEnemyTarget();
+        FlipTowardsTarget();
+    }
+
+    public void GetCurrentEnemyTarget()
+    {
+        if (_enemies.Count <= 0)
+        {
+            CurrentEnemyTarget = null;
+            return;
+        }
+
+        CurrentEnemyTarget = _enemies[0];
+    }
+
+    private void FlipTowardsTarget()
+    {
+        if (CurrentEnemyTarget == null)
+        {
+            return;
+        }
+
+        if (transform.position.x > CurrentEnemyTarget.transform.position.x)
+        {
+            _spriteRenderer[1].flipX = false;
+        }
+        else
+        {
+            _spriteRenderer[1].flipX = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy newEnemy = other.GetComponent<Enemy>();
+            _enemies.Add(newEnemy);
+        }
     }
 
     private void OnDrawGizmos()
